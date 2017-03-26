a77033e92056d00e33419da375a59bdcadc77f78
Traci Armstrong <traciarms@gmail.com>
2017-03-25 21:20:15 -0700

    added more unittests also added readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# recurring_events
+## Description
+
+The user is able to Create Events, Update Events, List All Events and Delete
+Events.
+
+## Details
+
+The delivery date is calculated based on the given start date, and avoiding
+any weekends or holidays.  If by avoiding weekends or holidays the deliver_date
+if found to be prior to the start date, the deliver date will be advanced to
+the following month.
+
+The Event detail page lists the event and the next 4 upcoming deliveries.
+This method calculates those next deliveries using the number of months
+between deliveries and watches for end of month, end of year and any days that
+are out of range for the month.  In other words it shouldn't produce any
+weekend or holiday days or invalid days.
+
+I wasn't sure how to use the number of days offset, so I don't think I used
+it in any of the calculations.  The user can enter it and it is being stored
+with the event, but again, I didn't see how to use it in the calculations.
+
+### Deliverables
+
+* A zip file from the GitHub repo
+
+### Execute
+
+* To run first:
+ pip install requirments.txt
+./manage.py runserver
+visit the link http://127.0.0.1:8000/ to create an event
+visit http://127.0.0.1:8000/all to view all events and then subsequently edit
+and delete events.
+
+
--- true_app/tests.py
+++ true_app/tests.py
@@ -1,28 +1,112 @@
+import datetime
+import unittest
+
 from django.test import TestCase
 
 from django.urls import reverse
 
 from true_app.models import Event
+from true_app.views import EventDetail, AllEvents
+from django.test import RequestFactory
 
 
-class CreateEventTests(TestCase):
-    def test_create_event_404(self):
-        test_event = Event.objects.create('name', '12-12-2017', 1, 1, 1)
-        test_event.save()
-
+class EventViewTests(TestCase):
+    def test_event_detail_404(self):
         url = reverse('detail', args=[100])
         response = self.client.get(url)
         self.assertEqual(response.status_code, 404, msg='not found')
 
-    def test_create_event(self):
-        test_event = Event.objects.create('name', '12-12-2017', 1, 1, 1)
+    def test_event_detail(self):
+        test_event = Event.objects.create(name='test',
+                                          start_date=datetime.date(day=12,
+                                                                   month=12,
+                                                                   year=2017),
+                                          )
         test_event.save()
-        event = Event.objects.\
-            create(name='name', start_date='12-12-2017',
-                   num_months_between_occ=1,
-                   day_of_month_of_occ=1,
-                   num_days_offset=1)
-        event.save()
 
-        response = self.client.get(reverse('detail'))
+        response = self.client.get(reverse('detail', args=[test_event.id]))
         self.assertEqual(response.status_code, 200, msg='found page')
+
+    def test_event_list(self):
+        test_event = Event.objects.create(name='test',
+                                          start_date=datetime.date(day=12,
+                                                                   month=12,
+                                                                   year=2017),
+                                          )
+        test_event.save()
+
+        response = self.client.get(reverse('event_list'))
+        self.assertEqual(response.status_code, 200, msg='found page')
+
+    def test_event_update(self):
+        test_event = Event.objects.create(name='test',
+                                          start_date=datetime.date(day=12,
+                                                                   month=12,
+                                                                   year=2017),
+                                          )
+        test_event.save()
+
+        response = self.client.get(reverse('update', args=[test_event.id]))
+        self.assertEqual(response.status_code, 200, msg='found page')
+
+
+class EventSaveTest(TestCase):
+
+    def test_event_save_same_month_weekend(self):
+        test_event = Event.objects.create(name='test',
+                                          start_date=datetime.date(day=12,
+                                                                   month=12,
+                                                                   year=2017),
+                                          num_months_between_occ=1,
+                                          day_of_month_of_occ=30,
+                                          num_days_offset=1
+                                          )
+        test_event.save()
+        self.assertTrue(test_event.delivery_date ==
+                        datetime.date(day=29,
+                                      month=12,
+                                      year=2017))
+
+    def test_event_save_next_month(self):
+        test_event = Event.objects.create(name='test',
+                                          start_date=datetime.date(day=31,
+                                                                   month=12,
+                                                                   year=2017),
+                                          num_months_between_occ=1,
+                                          day_of_month_of_occ=30,
+                                          num_days_offset=1
+                                          )
+        test_event.save()
+        self.assertTrue(test_event.delivery_date ==
+                        datetime.date(day=30,
+                                      month=1,
+                                      year=2018))
+
+    def test_event_save_next_month_weekend(self):
+        test_event = Event.objects.create(name='test',
+                                          start_date=datetime.date(day=28,
+                                                                   month=12,
+                                                                   year=2017),
+                                          num_months_between_occ=1,
+                                          day_of_month_of_occ=27,
+                                          num_days_offset=1
+                                          )
+        test_event.save()
+        self.assertTrue(test_event.delivery_date ==
+                        datetime.date(day=26,
+                                      month=1,
+                                      year=2018))
+
+
+class EventViewTestCase(unittest.TestCase):
+    def test_get(self):
+        # Setup name.
+        name = 'test event'
+        # Setup request and view.
+        request = RequestFactory().get('/fake-path')
+        view = AllEvents.as_view(template_name='event_list.html')
+        # Run.
+        response = view(request, name=name)
+        # Check.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.template_name[0], 'event_list.html')
